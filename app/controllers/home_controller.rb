f4c266510b17bab25b7c4c964ecef1c110646c20
Oleg Vodolazsky <oleg@supersaas.com>
2024-02-20 20:39:11 +0100

    Add basic app design, redirects, create templates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
+  end
 
+  def handle_digest
+    @link = Link.find(Shortener.base62to10(params[:digest])-CONVERSION_IDX_OFFSET)
+    if
+      redirect_to @link.destination_url, allow_other_host: true
+    else
+      render :link_not_found
+    end
   end
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,9 @@
 class LinksController < ApplicationController
   def	create
-    
+    digest = Shortener.base10to62(CONVERSION_IDX_OFFSET + (Link.last&.id || 1))
+    @link = Link.create(destination_url: params[:original_url], digest: digest)
+    @shortened_link = "#{request.base_url}/#{@link.digest}"
+    render :create
   end
+
 end
