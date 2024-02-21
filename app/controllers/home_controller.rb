3a801ceb09936caeaa3d16b19e5605fddd63e408
Oleg Vodolazsky <oleg@supersaas.com>
2024-02-21 14:24:58 +0100

    Fix handle_digest offset issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   end
 
   def handle_digest
-    @link = Link.find(Shortener.base62to10(params[:digest])-CONVERSION_IDX_OFFSET)
+    @link = Link.find(Shortener.base62to10(params[:digest])-CONVERSION_IDX_OFFSET+1)
     redirect_to @link.destination_url, allow_other_host: true
   end
 
