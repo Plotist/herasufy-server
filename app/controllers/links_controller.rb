bb47771887650bd5869e2eec73cd56d99f09d429
Oleg Vodolazsky <oleg@supersaas.com>
2024-02-21 14:39:36 +0100

    Fix an issue where first link generated would be redirected to a wrong page

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,8 @@
 class LinksController < ApplicationController
   def	create
-    digest = Shortener.base10to62(CONVERSION_IDX_OFFSET + (Link.last&.id || 1))
+
+    base10idx = Link.last ? (Link.last.id+1) : 1
+    digest = Shortener.base10to62(CONVERSION_IDX_OFFSET + base10idx)
     @link = Link.create(destination_url: params[:original_url], digest: digest)
     return render("home/index") unless @link.valid?
     @shortened_link = "#{request.base_url}/#{@link.digest}"
